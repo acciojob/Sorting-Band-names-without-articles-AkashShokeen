73452689a465c23f0777a677dac35f99a3fa5239
Akash Shokeen <117618540+AkashShokeen@users.noreply.github.com>
2023-08-15 12:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your code here
-let arr =['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+// let arr =['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 let mp={};
 for(let i =0;i<arr.length;i++){
 	let words = arr[i].split(" ");}
