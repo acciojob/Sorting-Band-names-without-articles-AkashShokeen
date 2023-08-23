fcd6a8a29ddf543c6dd3f91c90460256765ba251
Akash Shokeen <117618540+AkashShokeen@users.noreply.github.com>
2023-08-23 12:15:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-let mp={};
+let mp={ };
 for(let i=0;i<touristSpots.length;i++)
 	{ let words = touristSpots[i].split(" ");
 	let finalstring="";
 	for(let i =0;i<words.length;i++){
 		let currentword=words[i].toLowerCase();
 		if(currentword!=='a' && currentword!=='an' && currentword!=='the')
-		{finalstring += words[i]+ " ";
+		{ finalstring += words[i] + " ";
 		}}	
 mp[touristSpots[i]]=finalstring.trim();
 }
