d80e57267feea62277bf8ce49be2e228e25fad6b
Akash Shokeen <117618540+AkashShokeen@users.noreply.github.com>
2023-08-23 12:11:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-// 
-//your code here
-//your code here
-// const bands = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'Aerosmith'];
 
-// Define the strip function to remove common words from band names
-function strip(word) {
-const regex = /^(a |the |an )/i;
-return word.replace(regex, '').trim();
-}
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let mp={};
+for(let j=0;j<touristSpots.length;j++)
+	{ let words = touristSpots[j].split(" ");
+	let finalstring="";
+	for(let i =0;i<words.length;i++){
+		let currentword=words[i].toLowerCase();
+		if(currentword!=='a' && currentword!=='an' && currentword!=='the')
+			finalstring += words[i]+ " ";
+	
+mp[touristSpots[i]]=finalstring.trim();
+}}
+	
 
-// Sort the bands array using the strip function to ignore common words
-const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1);
 
-// Update the DOM with the sorted band names
-const bandList = document.getElementById('band');
-sortedBands.forEach(band => {
-const listItem = document.createElement('li');
-listItem.textContent = band;
-bandList.appendChild(listItem);
-});
